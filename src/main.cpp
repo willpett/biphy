5a1c5d8f92f7ce6a299820d85a84cebce582fd94
Walker Pett <will.pett@gmail.com>
2015-06-26 18:31:00 +0200

    added relaxed support for phylip files

---
+++ src/main.cpp
@@ -0,0 +1,400 @@
+#include "RbException.h"
+#include <iostream>
+#include <stdlib.h>
+#include <fstream>
+#include "util.h"
+#include "BiphyFreeTopology.h"
+
+using namespace std;
+
+int main (int argc, const char * argv[])
+{
+	string datafile = "None";
+	string treefile = "None";
+	string cvfile = "None";
+	string outgroupfile = "None";
+	string name = "";
+
+	BiphyFreeTopology::ModelType modeltype = BiphyFreeTopology::HOMOGENEOUS;
+	int mixture = 0;
+
+	BiphyFreeTopology::BranchPrior branchprior = BiphyFreeTopology::EXPONENTIAL;
+
+	int dgam = 4;
+
+	BiphyFreeTopology::RootPrior rootprior = BiphyFreeTopology::FREE;
+	double rootmin = 0.0;
+	double rootmax = 1.0;
+
+	int correction = RevBayesCore::NONE;
+
+	int every = 1;
+	int until = -1;
+	bool overwrite = false;
+
+	int numChains = 1;
+	int swapInterval = 1;
+	double delta = 0.05;
+	double sigma = 1;
+
+	bool saveall = false;
+	bool nexus = false;
+	bool ppred = false;
+	bool dolloMapping = false;
+	bool IIDmissing = false;
+
+	Biphy *chain = NULL;
+
+	try	{
+		if (argc == 2)	{
+			name = argv[1];
+			if (name == "-help" || name == "--help" || name == "-h")	{
+				throw(0);
+			}
+			if(!fexists(name+".stream")){
+				cerr << "run '" << name << "' does not exist\n";
+				exit(1);
+			}
+
+			chain = new BiphyFreeTopology(name);
+		}else if (argc == 1){
+			throw(0);
+		}else{
+
+			int i = 1;
+			while (i < argc)	{
+				string s = argv[i];
+
+				if (s == "-d")	{
+					i++;
+					datafile = argv[i];
+				}else if (s == "-f")	{
+					overwrite = true;
+				}
+				else if ((s == "-t") || (s == "-T"))	{
+					i++;
+					treefile = argv[i];
+				}
+				else if (s == "-cv")	{
+					i++;
+					cvfile = argv[i];
+				}
+				else if (s == "-o")	{
+					i++;
+					outgroupfile = argv[i];
+				}else if (s == "-s"){
+					saveall = true;
+				}else if (s == "-dgam"){
+					i++;
+					if (i == argc)	{
+						cerr << "error in command: -dgam <int>\n\n";
+						exit(1);
+					}
+					s = argv[i];
+					if (! IsInt(s))	{
+						cerr << "error in command: -dgam <int>\n\n";
+						exit(1);
+					}
+					dgam = atoi(argv[i]);
+				}else if (s == "-rigid"){
+					rootprior = BiphyFreeTopology::RIGID;
+				}
+				else if (s == "-nh")	{
+					modeltype = BiphyFreeTopology::HIERARCHICAL;
+				}
+				else if (s == "-e")	{
+					nexus = true;
+				}else if (s == "-i")	{
+					IIDmissing = true;
+				}else if (s == "-dollo"){
+					modeltype = BiphyFreeTopology::DOLLO;
+				}else if (s == "-map"){
+					dolloMapping = true;
+				}else if (s == "-h")	{
+					modeltype = BiphyFreeTopology::HOMOGENEOUS;
+				}else if (s == "-dpp")	{
+					modeltype = BiphyFreeTopology::DPP;
+				}else if (s == "-ldir")	{
+					branchprior = BiphyFreeTopology::DIRICHLET;
+				}else if (s == "-lexp")	{
+					branchprior = BiphyFreeTopology::EXPONENTIAL;
+				}else if (s == "-n")	{
+					i++;
+					if (i == argc)	{
+						cerr << "error in command: -n <int>\n\n";
+						exit(1);
+					}
+					s = argv[i];
+					if (! IsInt(s))	{
+						cerr << "error in command: -n <int>\n\n";
+						exit(1);
+					}
+					numChains = atoi(argv[i]);
+				}else if (s == "-u")	{
+					i++;
+					if (i == argc)	{
+						cerr << "error in command: -u <int>\n\n";
+						exit(1);
+					}
+					s = argv[i];
+					if (! IsInt(s))	{
+						cerr << "error in command: -u <int>\n\n";
+						exit(1);
+					}
+					correction = static_cast<RevBayesCore::CorrectionType>(atoi(argv[i]));
+					if(correction < 0 || correction > 15)	{
+						cerr << "error in command: -u <int>\n\n";
+						exit(1);
+					}
+				}else if (s == "-m")	{
+					i++;
+					if (i == argc)	{
+						cerr << "error in command: -m <int>\n\n";
+						exit(1);
+					}
+					s = argv[i];
+					if (! IsInt(s))	{
+						cerr << "error in command: -m <int>\n\n";
+						exit(1);
+					}
+					mixture = atoi(argv[i]);
+					if(mixture > 1){
+						modeltype = BiphyFreeTopology::MIXTURE;
+					}else{
+						modeltype = BiphyFreeTopology::HOMOGENEOUS;
+						mixture = 0;
+					}
+				}
+				else if (s == "-delta")	{
+					i++;
+					if (i == argc)	{
+						cerr << "error in command: -delta <float>\n\n";
+						exit(1);
+					}
+					s = argv[i];
+					if (! IsFloat(s))	{
+						cerr << "error in command: -delta <float>\n\n";
+						exit(1);
+					}
+					delta = atof(argv[i]);
+				}
+				else if (s == "-sigma")	{
+					i++;
+					if (i == argc)	{
+						cerr << "error in command: -sigma <float>\n\n";
+						exit(1);
+					}
+					s = argv[i];
+					if (! IsFloat(s))	{
+						cerr << "error in command: -sigma <float>\n\n";
+						exit(1);
+					}
+					sigma = atof(argv[i]);
+				}
+				else if (s == "-si")	{
+					i++;
+					if (i == argc)	{
+						cerr << "error in command: -si <int>\n\n";
+						exit(1);
+					}
+					s = argv[i];
+					if (!IsInt(s))	{
+						cerr << "error in command: -si <int>\n\n";
+						exit(1);
+					}
+					swapInterval = atof(argv[i]);
+				}else if (s == "-x")	{
+					i++;
+					if (i == argc)	{
+						cerr << "error in command: -x <every> <until>\n\n";
+						exit(1);
+					}
+					s = argv[i];
+					if (! IsInt(s))	{
+						cerr << "error in command: -x <every> <until>\n\n";
+						exit(1);
+					}
+					every = atoi(argv[i]);
+					i++;
+					if (i != argc)	{
+						s = argv[i];
+						if (IsInt(s))	{
+							until = atoi(argv[i]);
+						}
+						else	{
+							i--;
+						}
+					}
+				}else if (s == "-rp")	{
+					i++;
+					if (i == argc)	{
+						cerr << "error in command: -rp <min> <max>\n\n";
+						exit(1);
+					}
+					s = argv[i];
+					if (! IsFloat(s))	{
+						cerr << "error in command: -rp <min> <max>\n\n";
+						exit(1);
+					}
+					rootmin = atof(argv[i]);
+					i++;
+					if (i == argc)	{
+						cerr << "error in command: -rp <min> <max>\n\n";
+						exit(1);
+					}
+					s = argv[i];
+					if (! IsFloat(s))	{
+						cerr << "error in command: -rp <min> <max>\n\n";
+						exit(1);
+					}
+					rootmax = atof(argv[i]);
+					if(rootmin < 0 || rootmax > 1.0 || rootmin > rootmax){
+						cerr << "error in command: -rp <min> <max>\n\n";
+						exit(1);
+					}
+					rootprior = BiphyFreeTopology::TRUNCATED;
+				}else if (s == "-ppred")	{
+					ppred = true;
+				}else{
+					if (i != (argc -1))	{
+						throw(0);
+					}
+					name = argv[i];
+				}
+				i++;
+			}
+
+			if(name == ""){
+				cerr << "error: could not determine run name\n\n";
+				exit(1);
+			}
+
+			if(mixture > 1 && modeltype != BiphyFreeTopology::MIXTURE)
+				throw(0);
+
+			if(rootprior == BiphyFreeTopology::TRUNCATED && (modeltype > BiphyFreeTopology::HIERARCHICAL || modeltype == BiphyFreeTopology::DOLLO)){
+				cerr << "error: truncated root frequency only applies to -h or -nh\n\n";
+				exit(1);
+			}
+			if(rootprior == BiphyFreeTopology::RIGID && (modeltype < BiphyFreeTopology::HIERARCHICAL))
+				rootprior = BiphyFreeTopology::FREE;
+
+			if(modeltype == BiphyFreeTopology::DOLLO)
+				correction = (RevBayesCore::CorrectionType)(correction | RevBayesCore::NO_ABSENT_SITES);
+		}
+	}
+	catch(RbException &e){
+		throw(e);
+	}catch(...){
+
+		cerr << "biphy " << VERSION << "\n";
+		cerr << '\n';
+		cerr << "usage: biphy -d <data file> [-x <every> [<until>] ] <run name>\n";
+
+		cerr << "\nBranch frequency prior:\n";
+		cerr << "\t-dollo\t\tstochastic dollo model (enables -u 1)\n";
+		cerr << "\t-h\t\ttime-homogeneous beta model (default)\n";
+		cerr << "\t-nh\t\thierarchical beta model\n";
+		cerr << "\t-m <int>\tmixture with <int> beta components\n";
+		cerr << "\t-dpp\t\tdirichlet process prior\n";
+
+		cerr << "\nBranch length prior:\n";
+		cerr << "\t-lexp\t\thierarchical exponential prior (default)\n";
+		cerr << "\t-ldir\t\tcompound dirichlet prior\n";
+
+		cerr << "\nRates across sites prior:\n";
+		cerr << "\t-dgam <int>\tdiscrete gamma model with <int> categories (default: 4)\n";
+		cerr << "\t\t\t0 or 1 = constant rates model\n";
+
+		cerr << "\nCorrections for unobservable site patterns:\n";
+		cerr << "\t-u <int>\twhere <int> is one of:\n";
+		cerr << "\t\t0 = no site patterns have been omitted (default)\n";
+		cerr << "\t\t1 = constant absence sites have been removed\n";
+		cerr << "\t\t2 = constant presence sites have been removed\n";
+		cerr << "\t\t4 = singleton gains have been removed\n";
+		cerr << "\t\t8 = singleton losses have been removed\n";
+
+		cerr << "\tcombinations are achieved by adding the above values\n";
+		cerr << "\te.g.  3 = constant sites have been removed\n";
+		cerr << "\t     15 = uninformative sites have been removed\n";
+
+		cerr << "\nOptional constraints:\n";
+		cerr << "\t-t <file>\tfixed tree filename\n";
+		cerr << "\t-o <file>\toutgroup clade file\n";
+		cerr << "\t-rigid\trigid root frequency for heterogeneous models\n";
+		cerr << "\t-rp <min> <max>\ttruncate root frequency prior on (min,max) (-nh or -h only)\n";
+
+		cerr << "\nMCMCMC options:\n";
+		cerr << "\t-n <int>\tnumber of chains (default = 1)\n";
+		cerr << "\t-si <int>\tchain swap interval (default = 1)\n";
+		cerr << "\t-delta <float>\t(default = 0.1)\n";
+		cerr << "\t-sigma <float>\t(default = 1)\n";
+
+		cerr << "\nOutput options:\n";
+		cerr << "\t-s\t\tsave entire output (default: disabled)\n";
+		cerr << "\t-e\t\tsave nexus treefile (default: disabled)\n";
+
+		cerr << "\nModel-checking options:\n";
+		cerr << "\t-ppred\t\tposterior predictive simulation of tip frequencies\n";
+		cerr << "\t-cv <file>\tcross-validation test alignment\n";
+
+		cerr << "\nOther options:\n";
+		cerr << "\t-i\t\tIID missing data proportions across taxa (default: disabled)\n";
+		exit(1);
+	}
+
+	if(chain == NULL)
+		if(datafile != "None"){
+			if(datafile.at(0) != '.' && datafile.at(0) != '/'){
+				datafile = "./"+datafile;
+			}
+			if(treefile != "None" && treefile.at(0) != '.' && treefile.at(0) != '/'){
+				treefile = "./"+treefile;
+			}
+			if(outgroupfile != "None" && outgroupfile.at(0) != '.' && outgroupfile.at(0) != '/'){
+				outgroupfile = "./"+outgroupfile;
+			}
+
+			if(fexists(name+".param") && !overwrite){
+				cerr << "run '" << name << "' exists. use overwrite option -f\n";
+				exit(1);
+			}else if(overwrite){
+				remove((name+".stream").c_str());
+				remove((name+".param").c_str());
+				remove((name+".trace").c_str());
+				remove((name+".treelist").c_str());
+				if(nexus)
+					remove((name+".treelist.nex").c_str());
+			}
+
+			chain = new BiphyFreeTopology(name,datafile,treefile,outgroupfile,modeltype,branchprior,rootprior,correction,dgam,mixture,rootmin,rootmax,every,until,numChains,swapInterval,delta,sigma,saveall,nexus);
+		}else{
+			if(!fexists(name+".stream")){
+				cerr << "run '" << name << "' does not exist\n";
+				exit(1);
+			}
+			if(cvfile != "None" && cvfile.at(0) != '.' && cvfile.at(0) != '/'){
+				cvfile = "./"+cvfile;
+			}
+			if(ppred)
+				remove((name+".ppred").c_str());
+			if(cvfile != "None")
+				remove((name+".cv").c_str());
+			if(dolloMapping)
+				remove((name+".dollo.fa").c_str());
+			chain = new BiphyFreeTopology(name,cvfile,ppred,dolloMapping);
+		}
+
+	try
+	{
+		chain->init();
+		chain->run();
+	}
+	catch (RbException& e)
+	{
+		cerr << "Error:\t" << e.getMessage() << '\n';
+		exit(1);
+	}
+    
+    return 0;
+}
---
+++ src/main_multi.cpp
@@ -0,0 +1,337 @@
+#include "RbException.h"
+#include <iostream>
+#include <stdlib.h>
+#include <fstream>
+
+#include "BinaryCharEvoModel.h"
+#include "MultiBiphyRhoPi.h"
+#include "MultiBiphyLambdaMu.h"
+#include "util.h"
+
+using namespace std;
+
+int main (int argc, const char * argv[])
+{
+	string datafile = "None";
+	string treefile = "None";
+	string name = "";
+
+	enum Parameterization { LAMBDA_MU, RHO_PI };
+
+	MultiBiphy::ModelType modeltype = MultiBiphy::REVERSIBLE;
+	MultiBiphy::RatePrior A_prior 	= MultiBiphy::HOMOGENEOUS;
+	MultiBiphy::RatePrior B_prior 	= MultiBiphy::HOMOGENEOUS;
+	MultiBiphy::RatePrior A_species = MultiBiphy::HOMOGENEOUS;
+	MultiBiphy::RatePrior B_species = MultiBiphy::HOMOGENEOUS;
+
+	Parameterization parameterization = LAMBDA_MU;
+	int AB = 0;
+
+	int dgam = 4;
+
+	int correction = RevBayesCore::NONE;
+
+	int every = 1;
+	int until = -1;
+	bool overwrite = false;
+
+	int numChains = 1;
+	int swapInterval = 1;
+	double delta = 0.05;
+	double sigma = 1;
+
+	bool saveall = false;
+	bool ancestral = false;
+
+	MultiBiphy *chain = NULL;
+
+	try	{
+		if (argc == 2)	{
+			name = argv[1];
+			if (name == "-help" || name == "--help" || name == "-h")	{
+				throw(0);
+			}
+			if(!fexists(name+".stream")){
+				cerr << "run '" << name << "' does not exist\n";
+				exit(1);
+			}
+
+			chain = new RevBayesCore::MultiBiphyRhoPi(name);
+			chain = new RevBayesCore::MultiBiphyLambdaMu(name);
+		}else if (argc == 1){
+			throw(0);
+		}else{
+
+			int i = 1;
+			while (i < argc)	{
+				string s = argv[i];
+
+				if (s == "-d")	{
+					i++;
+					datafile = argv[i];
+				}else if (s == "-f")	{
+					overwrite = true;
+				}else if (s == "-a")	{
+					ancestral = true;
+				}
+				else if ((s == "-t"))	{
+					i++;
+					treefile = argv[i];
+				}else if (s == "-s"){
+					saveall = true;
+				}else if (s == "-dgam"){
+					i++;
+					if (i == argc)	{
+						cerr << "error in command: -dgam <int>\n\n";
+						exit(1);
+					}
+					s = argv[i];
+					if (! IsInt(s))	{
+						cerr << "error in command: -dgam <int>\n\n";
+						exit(1);
+					}
+					dgam = atoi(argv[i]);
+				//}else if (s == "-rev")	{
+				//	modeltype = MultiBiphyRhoPi::REVERSIBLE;
+				//}else if (s == "-dollo"){
+				//	modeltype = MultiBiphyRhoPi::DOLLO;
+				}else if (s == "-rho_pi")	{
+					parameterization = RHO_PI;
+					i++;
+					if (i == argc)	{
+						cerr << "error in command: -rho_pi <int>\n\n";
+						exit(1);
+					}
+					s = argv[i];
+					if (! IsInt(s))	{
+						cerr << "error in command: -rho_pi <int>\n\n";
+						exit(1);
+					}
+					AB = atoi(argv[i]);
+				}else if (s == "-lambda_mu")	{
+					parameterization = LAMBDA_MU;
+					i++;
+					if (i == argc)	{
+						cerr << "error in command: -lambda_mu <int>\n\n";
+						exit(1);
+					}
+					s = argv[i];
+					if (! IsInt(s))	{
+						cerr << "error in command: -lambda_mu <int>\n\n";
+						exit(1);
+					}
+					AB = atoi(argv[i]);
+				}else if (s == "-n")	{
+					i++;
+					if (i == argc)	{
+						cerr << "error in command: -n <int>\n\n";
+						exit(1);
+					}
+					s = argv[i];
+					if (! IsInt(s))	{
+						cerr << "error in command: -n <int>\n\n";
+						exit(1);
+					}
+					numChains = atoi(argv[i]);
+				}else if (s == "-u")	{
+					i++;
+					if (i == argc)	{
+						cerr << "error in command: -u <int>\n\n";
+						exit(1);
+					}
+					s = argv[i];
+					if (! IsInt(s))	{
+						cerr << "error in command: -u <int>\n\n";
+						exit(1);
+					}
+					correction = static_cast<RevBayesCore::CorrectionType>(atoi(argv[i]));
+					if(correction < 0 || correction > 15)	{
+						cerr << "error in command: -u <int>\n\n";
+						exit(1);
+					}
+				}
+				else if (s == "-delta")	{
+					i++;
+					if (i == argc)	{
+						cerr << "error in command: -delta <float>\n\n";
+						exit(1);
+					}
+					s = argv[i];
+					if (! IsFloat(s))	{
+						cerr << "error in command: -delta <float>\n\n";
+						exit(1);
+					}
+					delta = atof(argv[i]);
+				}
+				else if (s == "-sigma")	{
+					i++;
+					if (i == argc)	{
+						cerr << "error in command: -sigma <float>\n\n";
+						exit(1);
+					}
+					s = argv[i];
+					if (! IsFloat(s))	{
+						cerr << "error in command: -sigma <float>\n\n";
+						exit(1);
+					}
+					sigma = atof(argv[i]);
+				}
+				else if (s == "-si")	{
+					i++;
+					if (i == argc)	{
+						cerr << "error in command: -si <int>\n\n";
+						exit(1);
+					}
+					s = argv[i];
+					if (!IsInt(s))	{
+						cerr << "error in command: -si <int>\n\n";
+						exit(1);
+					}
+					swapInterval = atof(argv[i]);
+				}else if (s == "-x")	{
+					i++;
+					if (i == argc)	{
+						cerr << "error in command: -x <every> <until>\n\n";
+						exit(1);
+					}
+					s = argv[i];
+					if (! IsInt(s))	{
+						cerr << "error in command: -x <every> <until>\n\n";
+						exit(1);
+					}
+					every = atoi(argv[i]);
+					i++;
+					if (i != argc)	{
+						s = argv[i];
+						if (IsInt(s))	{
+							until = atoi(argv[i]);
+						}
+						else	{
+							i--;
+						}
+					}
+				}else{
+					if (i != (argc -1))	{
+						throw(0);
+					}
+					name = argv[i];
+				}
+				i++;
+			}
+
+			if(name == ""){
+				cerr << "error: could not determine run name\n\n";
+				exit(1);
+			}
+
+			if(modeltype == RevBayesCore::MultiBiphyRhoPi::DOLLO)
+				correction = (RevBayesCore::CorrectionType)(correction | RevBayesCore::NO_ABSENT_SITES);
+
+			if(AB & 0x01)
+				A_prior = MultiBiphy::HIERARCHICAL;
+			if(AB & 0x02)
+				B_prior = MultiBiphy::HIERARCHICAL;
+			if(AB & 0x04)
+				A_species = MultiBiphy::HIERARCHICAL;
+			if(AB & 0x08)
+				B_species = MultiBiphy::HIERARCHICAL;
+		}
+	}
+	catch(RbException &e){
+		throw(e);
+	}catch(...){
+
+		cerr << "multibiphy " << VERSION << "\n";
+		cerr << '\n';
+		cerr << "usage: multibiphy -t <tree file> -d <data file> [-x <every> [<until>] ] <run name>\n";
+
+		//cerr << "\nModel type:\n";
+		//cerr << "\t-dollo\t\tstochastic dollo model (enables -u 1)\n";
+		//cerr << "\t-rev\t\treversible model (default)\n";
+
+		cerr << "\nPrior parameterization:\n";
+		cerr << "\t-rho_pi <int>\n";
+		cerr << "\t-lambda_mu <int>\n";
+		cerr << "\t\twhere -A_B <int> is one of:\n";
+		cerr << "\t\t0 = homogeneous A and B (default)\n";
+		cerr << "\t\t1 = heterogeneous A across trees\n";
+		cerr << "\t\t2 = heterogeneous B across trees\n";
+		cerr << "\t\t4 = heterogeneous A across species\n";
+		cerr << "\t\t8 = heterogeneous B across species\n";
+
+		cerr << "\tcombinations are achieved by adding the above values\n";
+		cerr << "\te.g.  3 = heterogeneous A,B across trees\n";
+		cerr << "\t     12 = heterogeneous A,B across species\n";
+
+		cerr << "\nCorrections for unobservable site patterns:\n";
+		cerr << "\t-u <int>\twhere <int> is one of:\n";
+		cerr << "\t\t0 = no site patterns have been omitted (default)\n";
+		cerr << "\t\t1 = constant absence sites have been removed\n";
+		cerr << "\t\t2 = constant presence sites have been removed\n";
+		cerr << "\t\t4 = singleton gains have been removed\n";
+		cerr << "\t\t8 = singleton losses have been removed\n";
+
+		cerr << "\tcombinations are achieved by adding the above values\n";
+		cerr << "\te.g.  3 = constant sites have been removed\n";
+		cerr << "\t     15 = uninformative sites have been removed\n";
+
+		cerr << "\nMCMCMC options:\n";
+		cerr << "\t-n <int>\tnumber of chains (default = 1)\n";
+		cerr << "\t-si <int>\tchain swap interval (default = 1)\n";
+		cerr << "\t-delta <float>\t(default = 0.1)\n";
+		cerr << "\t-sigma <float>\t(default = 1)\n";
+
+		cerr << "\nOutput options:\n";
+		cerr << "\t-a\tsimulate ancestral states (default: disabled)\n";
+		cerr << "\t-s\tsave entire output (default: disabled)\n";
+		exit(1);
+	}
+
+	if(chain == NULL)
+		if(datafile != "None" && treefile != "None"){
+			if(datafile.at(0) != '.' && datafile.at(0) != '/'){
+				datafile = "./"+datafile;
+			}
+			if(treefile.at(0) != '.' && treefile.at(0) != '/'){
+				treefile = "./"+treefile;
+			}
+
+			if(fexists(name+".param") && !overwrite){
+				cerr << "run '" << name << "' exists. use overwrite option -f\n";
+				exit(1);
+			}else if(overwrite){
+				remove((name+".stream").c_str());
+				remove((name+".param").c_str());
+				remove((name+".trace").c_str());
+			}
+
+
+			if(parameterization == LAMBDA_MU){
+				chain = new RevBayesCore::MultiBiphyLambdaMu(name,datafile,treefile,modeltype,A_prior,B_prior,A_species,B_species,ancestral,correction,dgam,every,until,numChains,swapInterval,delta,sigma,saveall);
+			}else{
+				chain = new RevBayesCore::MultiBiphyRhoPi(name,datafile,treefile,modeltype,A_prior,B_prior,A_species,B_species,ancestral,correction,dgam,every,until,numChains,swapInterval,delta,sigma,saveall);
+			}
+		}else{
+			if(!fexists(name+".stream")){
+				cerr << "run '" << name << "' does not exist\n";
+				exit(1);
+			}
+			if(parameterization == LAMBDA_MU)
+				chain = new RevBayesCore::MultiBiphyLambdaMu(name,ancestral);
+			else
+				chain = new RevBayesCore::MultiBiphyRhoPi(name,ancestral);
+		}
+
+	try
+	{
+		chain->init();
+    	chain->run();
+	}
+	catch (RbException& e)
+	{
+		cerr << "Error:\t" << e.getMessage() << '\n';
+		exit(1);
+	}
+    
+    return 0;
+}
